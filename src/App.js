877b7f5f2434c73c980882b831717b19e9c828e4
nikita verma <nikitaverma21@navgurukul.org>
2023-10-30 15:23:08 +0530

    Product

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Login from './component/login';
 import Header from "./component/Header";
+import Product from "./component/Product";
 
 function App() {
   return (
     <div className="App">
      <Login/>
      <Header/>
+     <Product/>
     </div>
   );
 }
